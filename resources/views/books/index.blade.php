59b074b00ad1c30a8a0fb86799ea94ab69654172
nipuweerasuriya94 <nrweerasuriya@outlook.com>
2026-07-31 04:53:07 -0500

    Adding the search function and filters for the books panel.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -4,8 +4,34 @@
 
     <h1 class="mb-10 text-2x1"></h1>
     
-    <form></form>
+    <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+        <input type="text" name="title" placeholder="Search by title" value="{{ request('title') }}" class="input h-10"/>
+        <input type="hidden" name="filter" value="{{ request('filter') }}"/>
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
+    </form>
     
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                ''=> 'Latest',
+                'popular_last_month'=> 'Popular Last Month',
+                'popular_last_6months' => 'Popular Last 6 Months',
+                'highest_rated_last_month' => 'Highest Rated Last Month',
+                'highest_rated_last_6months' => 'Highest Rated Last 6 Months'
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}" class="{{ request('filter') === $key || (request('filter')=== null && $key === '')  ? 'filter-item-active' : 'filter-item' }}"> 
+                {{-- Request helper function to identify the active filter item and apply the CSS --}}
+                {{-- "..." is the spread operator used to unpack array inside an array --}}
+                {{ $label }}
+            </a>
+        @endforeach
+    </div>
+
+
     <ul>
         @forelse ($books as $book)
             <li class="mb-4">
